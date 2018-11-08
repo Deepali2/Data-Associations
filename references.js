90e566567b49f90612179ff2cfa8edbc53a4600a
Deepali2 <hdgarg@gmail.com>
2018-11-08 15:13:20 -0800

    makes the code modular by separating it into different files

--- references.js
+++ references.js
@@ -1,50 +1,36 @@
 const mongoose = require("mongoose");
 const ObjectId = require("mongodb").ObjectID;
 mongoose.connect("mongodb://localhost:27017/blog-demo_2", {useNewUrlParser: true});
+const Post = require("./models/post");
+const User = require("./models/user");
 
-const postSchema = new mongoose.Schema({
-  title: String,
-  content: String
-});
-const Post = mongoose.model("Post", postSchema);
-
-const userSchema = new mongoose.Schema({
-  email: String,
-  name: String,
-  posts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Post'
-    }
-  ]
-});
-const User = mongoose.model("User", userSchema);
 
 //Part 3
-
-// Post.create({
-//   title: "Cook it this way",
-//   content: "Part 3 says that buy all the ingredients that you wish to put inside your body and make a burger"
-// }, function(err, post) {
-//   if (err) console.log(err);
-//   else {
-//     User.findOne({email: "bob@gmail.com"}, function(err, user) {
-//       if (err) console.log(err);
-//       else {
-//         user.posts.push(post); 
-//         user.save(function(err, data) {
-//           if (err) console.log(err);
-//           else console.log(data);
-//         }); 
-//       } 
-//     });
-//   }
-// });
-
-User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user) {
+console.log(User);
+console.log(Post);
+Post.create({
+  title: "Part 4 how to lose weight",
+  content: "Stop eating"
+}, function(err, post) {
   if (err) console.log(err);
-  else console.log(user);
+  else {
+    User.findOne({email: "bob@gmail.com"}, function(err, user) {
+      if (err) console.log(err);
+      else {
+        user.posts.push(post); 
+        user.save(function(err, data) {
+          if (err) console.log(err);
+          else console.log(data);
+        }); 
+      } 
+    });
+  }
 });
+
+// User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user) {
+//   if (err) console.log(err);
+//   else console.log(user);
+// });
 
 //Part2
 // Post.create({
