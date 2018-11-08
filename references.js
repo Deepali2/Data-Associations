5c82c1ea70d762a68240b5c841c330c7567e1e16
Deepali2 <hdgarg@gmail.com>
2018-11-08 09:00:30 -0800

    uses .populate and .exec to find a post

--- references.js
+++ references.js
@@ -20,26 +20,53 @@ const userSchema = new mongoose.Schema({
 });
 const User = mongoose.model("User", userSchema);
 
+//Part 3
 
-Post.create({
-  title: "How to cook the best burger",
-  content: "buy and eat it"
-}, function(err, post) {
+// Post.create({
+//   title: "Cook it this way",
+//   content: "Part 3 says that buy all the ingredients that you wish to put inside your body and make a burger"
+// }, function(err, post) {
+//   if (err) console.log(err);
+//   else {
+//     User.findOne({email: "bob@gmail.com"}, function(err, user) {
+//       if (err) console.log(err);
+//       else {
+//         user.posts.push(post); 
+//         user.save(function(err, data) {
+//           if (err) console.log(err);
+//           else console.log(data);
+//         }); 
+//       } 
+//     });
+//   }
+// });
+
+User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user) {
   if (err) console.log(err);
-  else {
-    User.findOne({email: "bob@gmail.com"}, function(err, user) {
-      if (err) console.log(err);
-      else {
-        user.posts.push(post); 
-        user.save(function(err, data) {
-          if (err) console.log(err);
-          else console.log(data);
-        }); 
-      } 
-    });
-  }
+  else console.log(user);
 });
 
+//Part2
+// Post.create({
+//   title: "How to cook the best burger",
+//   content: "buy and eat it"
+// }, function(err, post) {
+//   if (err) console.log(err);
+//   else {
+//     User.findOne({email: "bob@gmail.com"}, function(err, user) {
+//       if (err) console.log(err);
+//       else {
+//         user.posts.push(post); 
+//         user.save(function(err, data) {
+//           if (err) console.log(err);
+//           else console.log(data);
+//         }); 
+//       } 
+//     });
+//   }
+// });
+
+//Part 1
 // User.create({
 //   email: "bob@gmail.com",
 //   name: "Bob Belcher"
